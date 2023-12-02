5136439050270c98bf5d46d6300b61a72336c2e8
23usman <133726485+23usman@users.noreply.github.com>
2023-12-02 19:19:10 +0700

    Membuat Form Login

---
+++ UsmanSyapotro_2110010642/UsmanSyapotro_2110010642/Unit1.pas
@@ -0,0 +1,71 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Button1: TButton;
+    Button2: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+  username:string;
+  password:string;
+  level:string;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+begin
+ username := Edit1.Text;
+ password := Edit2.Text;
+
+  zqry := TZQuery.Create(nil);
+  try
+    zqry.Connection := con;
+    zqry.SQL.Text := 'SELECT level FROM user WHERE username = :username AND password = :password';
+    zqry.ParamByName('username').AsString := username;
+    zqry.ParamByName('password').AsString := password;
+    zqry.Open;
+
+    if not zqry.IsEmpty then
+    begin
+      level := zqry.FieldByName('level').AsString;
+      if level = 'admin' then
+      begin
+        // Pengguna berhasil login sebagai admin
+        ShowMessage('login berhasil');
+        Form2.Show;
+      end;
+    end
+    else
+    begin
+      // Informasi login tidak valid
+      ShowMessage('username atau password salah');
+    end;
+  finally
+    zqry.Free;
+  end;
+end;
+end;
+
+end.
+ 
